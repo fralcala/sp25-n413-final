7a58baa7cafda3756c0b6985685b40a2451b74e2
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2025-04-21 15:57:06 -0400

    Front page and small adjustments with nav and font

--- wp-content/themes/finalproject/functions.php
+++ wp-content/themes/finalproject/functions.php
@@ -1,15 +1,1 @@
 <?php
-
-function init_edit_css()
-{
-    add_editor_style("assets/css/styles.css");
-}
-
-add_action("after_setup_theme", "init_edit_css");
-
-function init_view_css()
-{
-    wp_enqueue_style("group-project-style", get_stylesheet_directory_uri() . "/assets/css/styles.css");
-}
-
-add_action("wp_enqueue_scripts", "init_view_css");
